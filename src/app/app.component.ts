8c9acec98a67d82e9b374d500d62526856a60bc6
digitalTrilunaire <Trilunaire@live.fr>
2018-02-22 14:43:30 +0100

    Work on recipe home, add single recipe page, modify colors
    Add some static data for the recipe.
    Now we use an ion-card for the display of recipes.
    Add some colors on src/theme/variables to fit with
    material design.
    The tool from https://material.io/tools/color/ helps me
    Add a new static page to test the single recipe design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,14 @@ import { Recipe_WelcomePage } from '../pages/recipe_welcome/recipe_welcome';
 import { ProductsPage } from '../pages/products/products';
 import { MealPage } from '../pages/meal/meal';
 import { SettingsPage } from '../pages/settings/settings';
-
+import { BasketPage } from '../pages/basket/basket';
+import { SingleRecipePage } from '../pages/single-recipe/single-recipe';
 
 @Component({
   templateUrl: 'app.html'
 })
 
 export class MyApp {
-
 	@ViewChild(Nav) nav: Nav;
   rootPage:any = HomePage;
 	pages: Array<{title: string, component: any, icon: string }>;
@@ -26,12 +26,14 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.backgroundColorByHexString('#007713');
+			statusBar.show();
       splashScreen.hide();
 
 			//this.badge.set(5);
 
 			this.pages = [
+<<<<<<< HEAD
 <<<<<<< HEAD
       	{ title: 'Accueil', component: HomePage, icon: 'home', numberBadge: "7" },
 				{ title: 'Recettes', component: Recipe_WelcomePage, icon: 'bookmarks', numberBadge: "7" },
@@ -45,6 +47,18 @@ export class MyApp {
 				{ title: 'Repas', component: MealPage, icon: 'restaurant' },
 				{ title: 'Paramètres', component: SettingsPage, icon: 'settings' }
 >>>>>>> 48b4af2... Change the main view the cart item. Remove padding in the products to have a better view.
+=======
+      	{ title: 'Accueil', component: HomePage, icon: 'home'},
+				{ title: 'Recettes', component: Recipe_WelcomePage, icon: 'bookmarks'},
+				{ title: 'Produits', component: ProductsPage, icon: 'nutrition'},
+				{ title: 'Repas', component: MealPage, icon: 'restaurant'},
+<<<<<<< HEAD
+				{ title: 'Panier', component: BasketPage, icon: 'cart'}
+>>>>>>> 16f768e... Modify recipe ui
+=======
+				{ title: 'Panier', component: BasketPage, icon: 'cart'},
+				{ title: 'RecetteTest', component: SingleRecipePage, icon: 'cart'}
+>>>>>>> 1a1db1c... Improve recipe ui. Add some colors on src/theme/variables to fit with material design. Add a new page to test a single recipe design
     	];
 
     });
