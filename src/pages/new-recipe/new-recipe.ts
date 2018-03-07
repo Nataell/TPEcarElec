fc19a6ca6ff8249c0455ed882dea73b4abab3afe
digitalTrilunaire <Trilunaire@live.fr>
2018-03-07 23:19:42 +0100

    Improve new recipe ui with ingredients and steps. Just work as UI now. Bug on the last step when we want to change the order

--- src/pages/new-recipe/new-recipe.ts
+++ src/pages/new-recipe/new-recipe.ts
@@ -14,10 +14,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'new-recipe.html',
 })
 export class NewRecipePage {
-	useImage: bool;
+	useImage: boolean;
+	m_ingredientList: Array<{name: string, quantity: string}>;
+	m_stepList: Array<string>;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 		this.useImage=false;
+		this.m_ingredientList = [
+			{name: "Pain de mie", quantity: "2 tranches"},
+			{name: "Œufs", quantity: "1"},
+			{name: "Jambon", quantity: "1 tranche"},
+			{name: "fromage de chevre", quantity: "3 tranches"},
+			{name: "gruillère rapé", quantity: "50 g"}
+		];
+		this.m_stepList = [
+			"beurrez votre tranche de pain", "étalez la tranche de jambon sur votre tranche de pain", "placez vos trois tranches de fromage de chevre sur votre tranche de jambon", "fermez votre croque madame avec l'autre tranche de pain de mie", "ajouter du gruilère sur le dessus", "Vous pouvez ajouter l'œuf sur le pain du dessus", "Enfournez pendant 7 minutes"
+		];
   }
 
   ionViewDidLoad() {
