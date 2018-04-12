9120681ad920c5fcb81ee8da284d26f2c60598a5
digitalTrilunaire <Trilunaire@live.fr>
2018-04-12 13:05:10 +0200

    Improve recipeCreation UI, change app description and author
    Change the app description and author in package.json.
    For the recipe creation, add field verification (in javascript,
    must be done in backend). Show some field depending of the conditions
    Add local dynamic UI for the ingredients and recipe.

--- src/pages/new-recipe-steps/new-recipe-steps.ts
+++ src/pages/new-recipe-steps/new-recipe-steps.ts
@@ -17,8 +17,10 @@ import { NavController, Navbar } from 'ionic-angular';
 export class NewRecipeStepsPage {
   m_stepList: Array<string>;
   @ViewChild(Navbar) navBar: Navbar;
+  m_step;
 
   constructor(public navCtrl: NavController) {
+    this.m_step = "";
     this.m_stepList = [
       "beurrez votre tranche de pain", "étalez la tranche de jambon sur votre tranche de pain", "placez vos trois tranches de fromage de chevre sur votre tranche de jambon", "fermez votre croque madame avec l'autre tranche de pain de mie", "ajouter du gruilère sur le dessus", "Vous pouvez ajouter l'œuf sur le pain du dessus", "Enfournez pendant 7 minutes"
     ];
@@ -30,4 +32,30 @@ export class NewRecipeStepsPage {
     }
   }
 
+  goToRecipeValidation() {
+    console.log("validate recipe")
+  }
+
+  addNewStep(){
+    console.log("Adding new step");
+    if(this.m_step!=""){
+      var new_step = this.m_step;
+      this.m_stepList.push(new_step);
+      this.m_step = "";
+    }
+    else{
+      console.log("invalid data");
+      //display alert
+    }
+  }
+
+  removeStep(step){
+    var stepIndex = this.m_stepList.indexOf(step);
+    this.m_stepList.splice(stepIndex,1);
+  }
+
+  modifyStep(step){
+    this.m_step = step;
+    removeStep(step);
+  }
 }
